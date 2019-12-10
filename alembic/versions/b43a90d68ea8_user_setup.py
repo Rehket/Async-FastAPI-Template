dd8606cf03e61a24b5247f37c14abf30de342e90
Rehket <aalbright425@gmail.com>
2019-12-09 21:40:56 -0500

    New Alembic!

---
+++ alembic/versions/b43a90d68ea8_user_setup.py
@@ -0,0 +1,24 @@
+"""User Setup
+
+Revision ID: b43a90d68ea8
+Revises: 
+Create Date: 2019-12-09 21:40:25.911282
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b43a90d68ea8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
